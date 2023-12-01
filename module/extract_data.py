5d49ab0cfeb036ff4a580a9baab4d30a8a41096a
RodneyAndresPerez <rodneyp@uw.edu>
2023-12-01 12:56:09 -0800

    renamed functions to correspond w/ data (e.g., extract_data.kosko())

--- module/extract_data.py
+++ module/extract_data.py
@@ -2,11 +2,10 @@
 This module has a function to extract .csv files from another repository named data.
 """
 # It's only extracting kosko data
-# Need revise function to pull any data... Something like
-# def extract_data("data name")
+# Not sure if it extracts A2EI
 import os
 
-def extract_data():
+def kosko():
     """
     This function extracts kosko data from the /data/kosko repository.
     It interacts with other functions in different modules to be sorted
@@ -22,3 +21,20 @@ def extract_data():
     kosko_data = sorted(kosko_data)
     # add test pulls only .csv data
     return kosko_data
+
+def a2ei():
+    """
+    This function extracts kosko data from the /data/A2EI repository.
+    It interacts with other functions in different modules to be sorted
+    for analysis.
+    """
+    os.chdir(('../data/A2EI'))
+    files = os.listdir()
+    a2ei_data = []
+    for file in files:
+        if ".csv" in file:
+            a2ei_data.append(file)
+    # Sorts list of .csv data
+    a2ei_data = sorted(a2ei_data)
+    # add test pulls only .csv data
+    return a2ei_data
