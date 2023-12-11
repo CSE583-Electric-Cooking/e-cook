8f2dcf64b3b8789a9549710f2bc9b65e6eee5436
Eric Behling <ericbehl@uw.edu>
2023-12-11 15:05:54 -0800

    Proper formatting

--- sparkboard/plotting.py
+++ sparkboard/plotting.py
@@ -1,10 +1,39 @@
 """
-PLaceholder module docustrings
+This file provides an object oriented structure for the generation of plots to be embedded
+into the dashboard.py framework. Two classes are defined as such:
+
+    PlotTimeSeries: Class to generate time series plots
+    PlotSurvey: Class to generate bar graphs of filtered survey data
+
+
+Depencies:
+    plotly.subplots: Framework to make complex multi layer figures
+    plotly.graph_objs: Framework to populate subplots with either time series or bar graphs
+
+For example usage see dashboard.py
 """
 from plotly.subplots import make_subplots
 import plotly.graph_objs as go
 
 class PlotTimeSeries:
+    """
+    This class is designed to create time series plots for different data sources using Plotly.
+
+    __init__:
+        Constructs with the following objects:
+
+        dff (DataFrame): The pandas DataFrame containing the time series data.
+        columns (list): A list of column names from the DataFrame to be plotted.
+        selected_data_source (str): A string representing the selected data source ('kosko'/'a2ei').
+        kosko_logic (bool): A boolean representing specific logic for the 'kosko' data source.
+
+    __getattr__:
+        String based logic on input to correctly navigate to either the generic kosko or ae2i plot
+    
+        name (string): Input for get attribute
+
+    Inheritance: None
+    """
     def __init__(self,dff,columns,selected_data_source, kosko_status):
         self.dff = dff
         self.columns = columns
@@ -12,6 +41,12 @@ class PlotTimeSeries:
         self.kosko_logic = kosko_status != "ONOFF"
 
     def dash_plot(self):
+        """
+        Generates a Plotly subplot for time series data.
+
+        Returns:
+            go.Figure: A Plotly figure object containing the generated subplot.
+        """
         fig = make_subplots(rows=len(self.columns), cols=1, subplot_titles=self.columns)
         background_color = '#282828'
         paper_color = '#343a40'
@@ -32,10 +67,21 @@ class PlotTimeSeries:
         return fig
 
     def kosko(self, fig, col, i):
+        """
+        Adds time series traces specific to the 'kosko' data source to the provided figure.
+
+        Args:
+            fig (go.Figure): The Plotly figure object to which traces are added.
+            col (str): The name of the column to plot.
+            i (int): The index representing the row position in the subplot layout.
+
+        Returns:
+            go.Figure: The updated figure with 'kosko' specific traces added.
+        """
         if self.kosko_logic:
             fig.add_trace(
                 go.Scatter(x=self.dff["TIME"], y=self.dff[col], mode='lines', name=col,
-                        line=dict(width=2), marker=dict(size=1)),
+                        line={"width": 2}, marker={"size": 1}),
                 row=i, col=1
             )
         else:
@@ -43,23 +89,34 @@ class PlotTimeSeries:
             y_on = self.dff[col][self.dff["DEVICE STATUS"] == "OFF"]
             fig.add_trace(
                 go.Scatter(x=t_on, y=y_on, mode='lines', name=f"{col} OFF",
-                        line=dict(width=2), marker=dict(size=1)),
+                        line={"width": 2}, marker={"size": 1}),
                 row=i, col=1
             )
             t_off = self.dff["TIME"][self.dff["DEVICE STATUS"] == "ON"]
             y_off = self.dff[col][self.dff["DEVICE STATUS"] == "ON"]
             fig.add_trace(
                 go.Scatter(x=t_off, y=y_off, mode='lines', name=f"{col} ON",
-                        line=dict(width=2), marker=dict(size=1)),
+                        line={"width": 2}, marker={"size": 1}),
                 row=i, col=1
             )
         return fig
 
     def a2ei(self,fig,col,i):
+        """
+        Adds time series traces specific to the 'a2ei' data source to the provided figure.
+
+        Args:
+            fig (go.Figure): The Plotly figure object to which traces are added.
+            col (str): The name of the column to plot.
+            i (int): The index representing the row position in the subplot layout.
+
+        Returns:
+            go.Figure: The updated figure with 'a2ei' specific traces added.
+        """
         if col != "POWER FACTOR":
             fig.add_trace(
                 go.Scatter(x=self.dff["TIME"], y=self.dff[col], mode='lines', name=col,
-                        line=dict(width=2), marker=dict(size=1)),
+                        line={"width": 2}, marker={"size": 1}),
                 row=i, col=1
             )
         else:
@@ -67,23 +124,33 @@ class PlotTimeSeries:
             y_data = self.dff[col][self.dff[col] != 0]
             fig.add_trace(
                 go.Scatter(x=t_data, y=y_data, mode='lines', name=col,
-                        line=dict(width=2), marker=dict(size=1)),
+                        line={"width": 2}, marker={"size": 1}),
                 row=i, col=1
             )
         return fig
 
     def __getattr__(self, name):
         if name.startswith("kosko"):
-            def method(fig, col, i):
+            def method_kosko(fig, col, i):
                 return self.kosko(fig, col, i)
-            return method
+            return method_kosko
         if name.startswith("a2ei"):
-            def method(fig, col, i):
+            def method_ae2i(fig, col, i):
                 return self.a2ei(fig, col, i)
-            return method
+            return method_ae2i
         raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
 
 class PlotSurvey:
+    """
+    This class is designed for creating survey data plots using Plotly.
+
+    __init__:
+        Constructs with the following objects:
+
+        dff (DataFrame): The pandas DataFrame containing the survey data.
+        groups (list): A list of groups/categories identified in the survey data.
+        grouped_data (dict): A dictionary grouping the survey data by categories.
+    """
     def __init__(self, dff, survey_selection):
         self.dff = dff[dff["community_name"] == survey_selection]
         self.groups = sorted(set(col.split("/")[0] for col in self.dff.columns if "/" in col))
@@ -93,6 +160,12 @@ class PlotSurvey:
         self.grouped_data = {group: self.dff[column_func(group)] for group in self.groups}
 
     def dash_plot(self):
+        """
+        Generates a Plotly subplot for survey data.
+
+        Returns:
+            go.Figure: A Plotly figure object containing the generated subplot for the survey data.
+        """
         fig = make_subplots(rows=len(self.grouped_data),
                             cols=1,
                             subplot_titles=[g.capitalize() for g in self.groups])
@@ -110,7 +183,18 @@ class PlotSurvey:
         return fig
 
     def bar_graph(self,fig,data,i):
-        if (data == 0).all().all():
+        """
+        Adds bar graph to the provided subplot for a specific survey data group.
+
+        Args:
+            fig (go.Figure): The Plotly figure object to which bar plot traces are added.
+            data (DataFrame): The pandas DataFrame containing data for the specific survey group.
+            i (int): The index representing the row position in the subplot layout.
+
+        Returns:
+            go.Figure: The updated figure with bar plot traces added for the survey group.
+        """
+        if (data == 0).all().all(): # Logical check, displays an empty bar graph
             fig.add_trace(
                 go.Bar(x=[], y=[], name="No Data"),
                 row=i, col=1
